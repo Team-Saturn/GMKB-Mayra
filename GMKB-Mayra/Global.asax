95c3be96e883f8a6b2bfe473038054a27e7a6c9f
am934743 <am934743@gmail.com>
2015-03-30 21:36:47 -0500

    Init Commit
    Init Commit

---
+++ GMKB-Mayra/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GMKB_Mayra.MvcApplication" Language="C#" %>
---
+++ GMKB-Mayra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GMKB_Mayra.Startup))]
+namespace GMKB_Mayra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
